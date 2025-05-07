d055b9fbcba235859bacdb6b8162277f551b552f
Marc Fernández <marcausente@gmail.com>
2025-05-07 18:46:15 +0200

    FEAT: Dungon Model especificado

--- lib/models/dungeon_model.dart
+++ lib/models/dungeon_model.dart
@@ -1,13 +1,17 @@
 class Dungeon {
   final String title;
   final String description;
-  final Map<String, String> info;
+  final String expansion;
+  final String level;
+  final String location;
   final List<Boss> bosses;
 
   Dungeon({
     required this.title,
     required this.description,
-    required this.info,
+    required this.expansion,
+    required this.level,
+    required this.location,
     required this.bosses,
   });
 }
@@ -15,13 +19,11 @@ class Dungeon {
 class Boss {
   final String name;
   final String image;
-  final String difficulty;
   final String mechanics;
 
   Boss({
     required this.name,
     required this.image,
-    required this.difficulty,
     required this.mechanics,
   });
 } 
